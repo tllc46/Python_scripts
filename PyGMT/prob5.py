2dde3485467b23be373b4b4951e7ee83cb529fff
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-05-17 13:21:42 +0900

    Update prob5.py

--- PyGMT/prob5.py
+++ PyGMT/prob5.py
@@ -13,8 +13,8 @@ pygmt.config(FORMAT_GEO_MAP="D",MAP_FRAME_TYPE="plain")
 fig.coast(frame=["WeSn","xa0.2-0.1f0.1","y0.15"],projection="merc/10c",region=[126.1,127,33.15,33.6],shorelines=True)
 color_range=pygmt.info(data=df["residual"],nearest_multiple=0.05)[:2]
 pygmt.makecpt(cmap="polar",series=color_range)
-fig.plot(x=df.loc[df["status"]=="O","longitude"],y=df.loc[df["status"]=="O","latitude"],size=2*abs(df.loc[df["status"]=="O","residual"])+0.1,cmap=True,fill=df.loc[df["status"]=="O","residual"],style="cc",pen=True)
-fig.plot(x=df.loc[df["status"]=="X","longitude"],y=df.loc[df["status"]=="X","latitude"],style="+0.15c")
+fig.plot(x=df.loc[df["status"].isin("O"),"longitude"],y=df.loc[df["status"].isin("O"),"latitude"],size=2*abs(df.loc[df["status"].isin("O"),"residual"])+0.1,cmap=True,fill=df.loc[df["status"].isin("O"),"residual"],style="cc",pen=True)
+fig.plot(x=df.loc[df["status"].isin("X"),"longitude"],y=df.loc[df["status"].isin("X"),"latitude"],style="+0.15c")
 fig.text(x=df["longitude"],y=df["latitude"],text=df["name"],font="5p",justify="RB",offset="j0.1c",fill="white@40")
 fig.colorbar(frame=[0.05,"x+lresidual (s)"],position="JCB+w4c")
 
