5574afdae581bca59ee6302fb86ff52a4af9a23d
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-05-17 16:15:34 +0900

    Update prob6.py

--- PyGMT/prob6.py
+++ PyGMT/prob6.py
@@ -9,7 +9,7 @@ grid=pygmt.xyz2grd(data="depth_30km.txt",spacing=["469+n","325+n"],region=[125.6
 pygmt.makecpt(cmap="polar",reverse=True,series=[-0.2,0.2])
 fig.grdimage(grid=grid,frame=["WeSn","a0.5f0.1"],projection="merc/10c")
 fig.coast(shorelines="1p")
-fig.plot(x=df["longitude"],y=df["latitude"],fill="white",style="s0.15c",pen="0.75p")
+fig.plot(data=df[["longitude","latitude"]],fill="white",style="s0.15c",pen="0.75p")
 fig.plot(x=[125.62,127.5115],y=[33.35,33.35],pen=",,-")
 fig.text(position="RM",text="prob7",justify="RT",offset="j0.5c/0.7c")
 fig.colorbar(frame=[0.1,"x+ldVp (km/s)"],position="JCB+w8c")
