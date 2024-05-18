8d88eea0439fb910b337237ff47fc542551bb500
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-05-18 23:08:56 +0900

    Update prob8.py

--- PyGMT/prob8.py
+++ PyGMT/prob8.py
@@ -28,8 +28,8 @@ fig.basemap(frame=["lrbtZ+b","z15","z+ldepth (km)"],region=[125.62,127.5115,32.8
 fig.colorbar(frame=[0.1,"x+ldVp (km/s)"],position="JCB+w8c")
 
 #elevation plot
-grid_relief=pygmt.datasets.load_earth_relief(resolution="01m",region=[125.6,127.6,32.8,33.95])
+grid=pygmt.datasets.load_earth_relief(resolution="01m",region=[125.6,127.6,32.8,33.95])
 fig.shift_origin(yshift=f"{y_shift}c")
-fig.grdview(grid=0.001*grid_relief,cmap="geo",shading="+d",projection="merc/10c",zscale="1c",plane=0,surftype="s",region=[125.62,127.5115,32.8565,33.95,0,2],perspective=True) #must provide projection explicitly. colorbar initializes projection by -Jx1i
+fig.grdview(grid=0.001*grid,cmap="geo",shading="+d",projection="merc/10c",zscale="1c",plane=0,surftype="s",region=[125.62,127.5115,32.8565,33.95,0,2],perspective=True) #must provide projection explicitly. colorbar initializes projection by -Jx1i
 fig.basemap(frame=["WrStZ","a0.5f0.1","z1","z+lelevation (km)"],perspective=True)
 fig.savefig(fname="prob8.png")
