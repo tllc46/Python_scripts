8fdcc2ee28fdf016b471f1828365b286c8339dbe
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-05-18 12:23:14 +0900

    Update prob11.py

--- PyGMT/prob11.py
+++ PyGMT/prob11.py
@@ -11,8 +11,8 @@ lat_center=0.5*(lat_1+lat_2)
 df=pd.read_csv(filepath_or_buffer="KMA_events.txt",sep=" ")
 df_size=df[["longitude","latitude","magnitude"]]
 df_size_big=df_size.loc[df_size["magnitude"].isin([5.1,5.8,5.4])]
-df_size.loc[:,"magnitude"]*=0.05
-df_size_big.loc[:,"magnitude"]*=0.05
+df_size["magnitude"]*=0.05
+df_size_big["magnitude"]*=0.05
 
 depth_scale=0.1 #cm/km
 depth_max=25 #km
@@ -30,7 +30,7 @@ fig.plot(data=df_size_big,style="cc",pen="black")
 grid=pygmt.datasets.load_earth_relief(resolution="01m",region=[129,130,35,38])
 track=pygmt.grdtrack(grid=grid,profile=f"{lon_1}/{lat_1}/{lon_2}/{lat_2}+d+i0.01d")
 track=track[[2,3]]
-track.loc[:,3]*=0.001
+track[3]*=0.001
 height_range=pygmt.info(data=track[3],nearest_multiple=0.2)[:2]
 fig.shift_origin(xshift="0.3c",yshift="-2c")
 fig.plot(x=[0,track.iloc[-1,0]],y=[0,0],frame=["lEt","y1","y+lheight(km)"],fill="lightblue",projection="xy/6c/0.5c",close="+yb",region=[0,track.iloc[-1,0],height_range[0],height_range[1]])
@@ -40,8 +40,8 @@ fig.plot(data=track,fill="black",close="+yb")
 profile=pygmt.project(data=df[["longitude","latitude","depth","magnitude"]],center=[lon_1,lat_1],endpoint=[lon_2,lat_2],width=[-0.2,0.2])
 profile_size=profile[[4,2,3]]
 profile_size_big=profile_size.loc[profile_size[3].isin([5.1,5.8,5.4])]
-profile_size.loc[:,3]*=0.05
-profile_size_big.loc[:,3]*=0.05
+profile_size[3]*=0.05
+profile_size_big[3]*=0.05
 fig.shift_origin(yshift=f"-{y_shift}c")
 fig.plot(data=profile_size,frame=["WrS","x0.5","x+ldistance(@.)","ya10f5","y+ldepth(km)"],projection=f"xy/6c/-{depth_scale}c",region=[0,track.iloc[-1,0],0,depth_max],style="cc",pen="red")
 fig.plot(data=profile_size_big,style="cc",pen="black")
