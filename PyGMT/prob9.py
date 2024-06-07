5b2eb48781f5d907232363bc6f88e5cf5899ef4e
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-06-07 13:28:37 +0900

    Update prob9.py

--- PyGMT/prob9.py
+++ PyGMT/prob9.py
@@ -2,8 +2,6 @@ import pandas as pd
 import pygmt
 
 df=pd.read_csv(filepath_or_buffer="KMA_events.txt",sep=" ")
-df=df[["longitude","latitude","depth","magnitude"]]
-df["magnitude"]*=0.05
 
 fig=pygmt.Figure()
 pygmt.config(FORMAT_GEO_MAP="D")
@@ -12,7 +10,7 @@ series=pygmt.info(data=df["depth"],nearest_multiple=5)[:2]
 pygmt.makecpt(cmap="inferno",reverse=True,series=series)
 fig.coast(frame=["WeSn",2],land="gray",projection="merc/1.5c",region=region)
 fig.plot(data="faults.txt",pen="darkolivegreen")
-fig.plot(data=df,cmap=True,style="cc",pen="+cl")
+fig.plot(x=df["longitude"],y=df["latitude"],size=0.05*df["magnitude"],cmap=True,fill=df["depth"],style="cc",pen="+cl")
 fig.plot(x=[129.05,129.9],y=[35.55,37],pen="blue")
 fig.text(x=129.9,y=37.1,text="prob11",justify="LB")
 fig.colorbar(frame=[5,"x+ldepth(km)"],position="JCB+w5c")
