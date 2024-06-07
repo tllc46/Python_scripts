b5edaba8f1ed15c851e699eff772c27a1b4659bd
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-06-07 11:22:34 +0900

    Update prob6.py

--- PyGMT/prob6.py
+++ PyGMT/prob6.py
@@ -11,7 +11,7 @@ series=series[2:-1].split(sep="/")[:2]
 pygmt.makecpt(cmap="polar",reverse=True,series=series)
 fig.grdimage(grid=grid,frame=["WeSn","a0.5f0.1"],projection="merc/5c")
 fig.coast(shorelines=True)
-fig.plot(data=df[["longitude","latitude"]],fill="white",style="s0.12c",pen=True)
+fig.plot(x=df["longitude"],y=df["latitude"],fill="white",style="s0.12c",pen=True)
 fig.plot(x=[125.62,127.5115],y=[33.35,33.35],pen="dashed")
 fig.text(x=127.5115,y=33.35,text="prob7",justify="RT",offset="j0.1c")
 fig.colorbar(frame=[0.1,"x+ldVp (km/s)"],position="JCB+w4c")
