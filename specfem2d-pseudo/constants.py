b6dba02a9f667ced71df6d1bbae1e41c8caa0c7a
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-06-20 16:41:05 +0900

    Create constants.py

---
+++ specfem2d-pseudo/constants.py
@@ -0,0 +1,39 @@
+nproc=1
+nstep=10
+dt=0.1
+partition_type=3
+ngnod=4
+setup_with_binary_database=0
+model="default"
+save_model="default"
+    
+attenuation_viscoelastic=False
+attenuation_porofluid_part=False
+undo_attenuation_and_or_pml=False
+    
+nsources=1
+initialfield=False
+    
+pml_boundary_conditions=True
+nelem_pml_thickness=3
+rotate_pml_activate=False
+k_min_pml=1
+k_max_pml=1
+damping_change_factor_elastic=1
+pml_parameter_adjustment=False
+stacey_absorbing_conditions=False
+    
+nbmodels=1
+read_external_mesh=False
+    
+xmin_param=0 #xmin
+xmax_param=400 #xmax
+nx_param=80 #nx
+absorbbottom=True
+absorbright=True
+absorbtop=False
+absorbleft=True
+
+nbregions=1
+
+number_of_simultaneous_runs=1
