89523ee792ee7da563671283d94d026c01c332c9
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-06-21 22:56:00 +0900

    Update geophone_map.py

--- geophone_map.py
+++ geophone_map.py
@@ -1,16 +1,16 @@
 def parse_kml():
     from pykml import parser
 
+
+    namespace={"kml":"http://www.opengis.net/kml/2.2"}
+
     kml_file_name="Weonju.kml"
     kml_file=open(file=kml_file_name)
     doc=parser.parse(fileobject=kml_file)
     kml_file.close()
     root=doc.getroot()
-    for folder in root.Document.Folder:
-        if folder.name=="원주_지오폰_어레이_위치":
-            break
+    folder=root.Document.xpath("kml:Folder[kml:name='원주_지오폰_어레이_위치']",namespaces=namespace)[0]
 
-    namespace={"kml":"http://www.opengis.net/kml/2.2"}
     csv_file_name="Weonju.txt"
     csv_file=open(file=csv_file_name,mode="w")
 
