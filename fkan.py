a73f23ff6d3d40836f954619756895bda4a2119c
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-06-26 15:07:19 +0900

    Update fkan.py

--- fkan.py
+++ fkan.py
@@ -9,21 +9,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 d2km=180/(np.pi*6371)
-sll_x=-3 #slowness least x
-slm_x=5 #slowness max x
-sll_y=10 #slowness least y
-slm_y=20 #slowness max y
-sl_s=0.1 #slowness step
-frqlow=1 #frequency low
-frqhigh=2 #frequency high
-frqfoc=1.5 #frequency focusing
-ssm="incoherent" #signal subspace method, "coherent" or "incoherent"
-method="Bartlett" #beamforming method, "Bartlett" or "Capon" or "MUSIC"
+sll_x=-3 #최소 x slowness
+slm_x=5 #최대 x slowness
+sll_y=10 #최소 y slowness
+slm_y=20 #최대 y slowness
+sl_s=0.1 #slowness 단계
+frqlow=1 #최소 진동수
+frqhigh=2 #최대 진동수
+frqfoc=1.5 #focusing 진동수
+ssm="incoherent" #신호 부공간 방법, "coherent" or "incoherent"
+method="Bartlett" #beamforming 방법, "Bartlett" or "Capon" or "MUSIC"
 
 def fk_cssm(sl_x,sl_y):
     steer=np.exp(1j*2*np.pi*frqfoc*d2km*(sl_x*geometry[:,0]+sl_y*geometry[:,1]))
     tmp=np.matmul(scm,steer)
-    power=np.real(np.matmul(steer.conj(),tmp))
+    power=np.real(val=np.matmul(steer.conj(),tmp))
     return power
 
 def fk_issm(sl_x,sl_y):
@@ -37,64 +37,63 @@ def fk_issm(sl_x,sl_y):
         power+=tmp2
     return power
 
-np_fk_cssm=np.frompyfunc(fk_cssm,2,1)
-np_fk_issm=np.frompyfunc(fk_issm,2,1)
+np_fk_cssm=np.frompyfunc(fk_cssm,nin=2,nout=1)
+np_fk_issm=np.frompyfunc(fk_issm,nin=2,nout=1)
 
 #main
-#1. read data
+#1. data 읽기
 st=read(pathname_or_url="cutdata/24/Z/*",format="SAC",byteorder="little")
-#st=st[30:90]
 nch=len(st)
 
-#2. get array geometry
+#2. 지진계 중심으로부터 거리 vector
 for i in range(nch):
     st[i].stats.coordinates=AttribDict({"latitude":st[i].stats.sac.stla,"longitude":st[i].stats.sac.stlo,"elevation":-12345})
 geometry=get_geometry(stream=st,return_center=True)
 center=geometry[-1]
 geometry=geometry[:-1]
-_,_,baz=gps2dist_azimuth(st[0].stats.sac.evla,st[0].stats.sac.evlo,center[1],center[0])
-angle=0.5*np.pi-baz*np.pi/180
+_,_,baz=gps2dist_azimuth(lat1=st[0].stats.sac.evla,lon1=st[0].stats.sac.evlo,lat2=center[1],lon2=center[0])
+angle=0.5*np.pi-np.radians(baz)
 
-#3. get reference slowness
+#3. 참조 slowness 계산
 model=TauPyModel(model="ak135")
-arrivals=model.get_travel_times_geo(st[0].stats.sac.evdp,st[0].stats.sac.evla,st[0].stats.sac.evlo,center[1],center[0],phase_list=["P","Pn"])
+arrivals=model.get_travel_times_geo(source_depth_in_km=st[0].stats.sac.evdp,source_latitude_in_deg=st[0].stats.sac.evla,source_longitude_in_deg=st[0].stats.sac.evlo,receiver_latitude_in_deg=center[1],receiver_longitude_in_deg=center[0],phase_list=["P","Pn"])
 sl_P=arrivals[2].ray_param_sec_degree
 sl_Pn=arrivals[1].ray_param_sec_degree
 phase=sl_P
 
-#5. generate plan for rfft
+#4. rfft 준비
 nsamples=st[0].stats.npts
 fs=st[0].stats.sampling_rate
-nfft=next_pow_2(nsamples)
+nfft=next_pow_2(i=nsamples)
 deltaf=fs/nfft
 nlow=int(frqlow/deltaf+0.5)
 nhigh=int(frqhigh/deltaf+0.5)
-nlow=max(1,nlow) #avoid using the offset
-nhigh=min(nfft//2-1,nhigh) #avoid using Nyquist
-nf=nhigh-nlow+1 #include upper and lower frequency
-frq=np.linspace(0,fs/2,nfft//2+1) #frequency range
+nlow=max(1,nlow) #최소 진동수가 0Hz보다는 커야 한다
+nhigh=min(nfft//2-1,nhigh) #최대 진동수가 Nyquist 진동수보다는 작아야 한다
+nf=nhigh-nlow+1 #최소 진동수와 최대 진동수도 포함
+frq=np.linspace(0,fs/2,nfft//2+1) #진동수 범위
 
-#6. rfft
-output=np.empty((nch,nf),dtype=np.complex128)
+#5. rfft
+output=np.empty(shape=(nch,nf),dtype=np.complex128)
 for i in range(nch):
     dat=st[i].data
-    output[i]=np.fft.rfft(dat,nfft)[nlow:nlow+nf]
+    output[i]=np.fft.rfft(a=dat,n=nfft)[nlow:nlow+nf]
 
-#7. computing the covariances of the signal at different receivers
+#6. 공분산 행렬 계산
 if ssm=="coherent":
-    #7-1. focus
+    #6-1. focus
     for i in range(nf):
         output[:,i]*=np.exp(1j*2*np.pi*(frqfoc-frq[nlow+i])*d2km*phase*(np.cos(angle)*geometry[:,0]+np.sin(angle)*geometry[:,1]))
 
-    #7-2. compute average covariance matrix
-    scm=np.empty((nch,nch),dtype=np.complex128)
+    #6-2. 공분산 행렬의 진동수에 대한 평균 계산
+    scm=np.empty(shape=(nch,nch),dtype=np.complex128)
     for i in range(nch):
         for j in range(i,nch):
             scm[i,j]=sum(output[i]*output[j].conj())
             if i!=j:
                 scm[j,i]=scm[i,j].conj()
 else:
-    scm=np.empty((nch,nch,nf),dtype=np.complex128)
+    scm=np.empty(shape=(nch,nch,nf),dtype=np.complex128)
     for i in range(nch):
         for j in range(i,nch):
             scm[i,j]=output[i]*output[j].conj()
@@ -103,48 +102,48 @@ else:
             if i!=j:
                 scm[j,i]=scm[i,j].conj()
 
-#8. apply methods
+#7. 공분산 행렬에 beamforming 방법 적용
 if ssm=="coherent":
     if method=="Capon":
-        w,z=np.linalg.eig(scm)
-        tmp=np.matmul(z,np.diag(1/w))
-        scm=np.matmul(tmp,np.conjugate(np.transpose(z)))
+        w,z=np.linalg.eig(a=scm)
+        tmp=np.matmul(z,np.diag(v=1/w))
+        scm=np.matmul(tmp,np.conjugate(np.transpose(a=z)))
     
     elif method=="MUSIC":
-        w,z=np.linalg.eig(scm)
-        ind=np.argsort(np.real(w))
+        w,z=np.linalg.eig(a=scm)
+        ind=np.argsort(a=np.real(val=w))
         ind=ind[:-1]
-        scm=np.matmul(z[:,ind],np.conjugate(np.transpose(z[:,ind])))
+        scm=np.matmul(z[:,ind],np.conjugate(np.transpose(a=z[:,ind])))
 else:
     if method=="Capon":
     #P(f)=1/(e.H R(f)^-1 e)
         for i in range(nf):
-           scm[:,:,i]=np.linalg.pinv(scm[:,:,i],rcond=1e-6)
+           scm[:,:,i]=np.linalg.pinv(a=scm[:,:,i],rcond=1e-6)
 
-#9. beamforming
-sl_x=np.arange(sll_x,slm_x,sl_s)
-sl_y=np.arange(sll_y,slm_y,sl_s)
+#8. beamforming
+sl_x=np.arange(stop=(slm_x-sll_x)/sl_s)*sl_s+sll_x
+sl_y=np.arange(stop=(slm_y-sll_y)/sl_s)*sl_s+sll_y
 sl_x,sl_y=np.meshgrid(sl_x,sl_y)
 if ssm=="coherent":
-    fks=np_fk_cssm(sl_x,sl_y)
-    fks=np.array(fks,dtype=float)
+    fks=np_fk_cssm(sl_x=sl_x,sl_y=sl_y)
+    fks=np.array(object=fks,dtype=float)
     if method=="Capon" or method=="MUSIC":
         fks=1/fks
 else:
-    fks=np_fk_issm(sl_x,sl_y)
-    fks=np.array(fks,dtype=float)
+    fks=np_fk_issm(sl_x=sl_x,sl_y=sl_y)
+    fks=np.array(object=fks,dtype=float)
 
-#10. find maximum index
-ix,iy=np.unravel_index(fks.argmax(),fks.shape)
+#9. find maximum index
+ix,iy=np.unravel_index(indices=fks.argmax(),shape=fks.shape)
 
-#11. plot
+#10. plot
 plt.figure()
-plt.set_cmap("Reds")
-plt.pcolormesh(sl_x,sl_y,fks)
-plt.scatter(sl_x[ix,iy],sl_y[ix,iy],c="green",s=200,marker="+")
-plt.scatter(sl_Pn*np.cos(angle),sl_Pn*np.sin(angle),c="blue",s=200,marker="+")
-plt.scatter(sl_P*np.cos(angle),sl_P*np.sin(angle),c="blue",s=200,marker="+")
-plt.xlabel("sl_x(s/°)")
-plt.ylabel("sl_y(s/°)")
-plt.grid()
+ax=fig.subplots()
+ax.pcolormesh(sl_x,sl_y,fks,cmap="Reds")
+ax.scatter(x=sl_x[ix,iy],y=sl_y[ix,iy],s=200,c="green",marker="+")
+ax.scatter(x=sl_Pn*np.cos(angle),y=sl_Pn*np.sin(angle),s=200,c="blue",marker="+")
+ax.scatter(x=sl_P*np.cos(angle),y=sl_P*np.sin(angle),s=200,c="blue",marker="+")
+ax.set_xlabel(xlabel="sl_x(s/°)")
+ax.set_ylabel(ylabel="sl_y(s/°)")
+ax.grid(visible=True)
 plt.show()
