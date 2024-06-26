74a13d9e90a415b8817b9dc90bec45020d3f357d
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-06-26 13:36:11 +0900

    Update specfem2d.py

--- specfem2d.py
+++ specfem2d.py
@@ -4,30 +4,30 @@ from os import mkdir
 import numpy as np
 from scipy.special import gamma,roots_jacobi,eval_legendre
 
-#constants
-rho=2700 #density [kg/m^3]
-cp=3000 #P wave velocity [m/s]
-cs=cp/np.sqrt(3) #S wave velocity [m/s]
+#상수
+rho=2700 #밀도 [kg/m^3]
+cp=3000 #P파 속도 [m/s]
+cs=cp/np.sqrt(3) #S파 속도 [m/s]
 
-xmin=0 #left [m]
-xmax=4000 #right [m]
-zmin=0 #bottom [m]
-zmax=3000 #top [m]
-nelem_x=80 #no. of spectral elements along x
-nelem_z=60 #no. of spectral elements along z
-nspec=nelem_x*nelem_z #no. of total spectral elements
-npgeo=(nelem_x+1)*(nelem_z+1) #no. of total nodes
+xmin=0 #격자 왼쪽 [m]
+xmax=4000 #격자 오른쪽 [m]
+zmin=0 #격자 아래 [m]
+zmax=3000 #격자 위 [m]
+nelem_x=80 #x축 상 요소 개수
+nelem_z=60 #z축 상 요소 개수
+nspec=nelem_x*nelem_z #총 요소 개수
+npgeo=(nelem_x+1)*(nelem_z+1) #총 격자점 개수
 
-ngllx=5 #no. of gll points along x
-ngllz=5 #no. of gll points along z
-nxyz=ngllx*ngllz #total no. of gll points in 1 spectral element
-ntot=nxyz*nspec #total no. of gll points (includes overlap)
-nglob=(nelem_x*(ngllx-1)+1)*(nelem_z*(ngllz-1)+1) #no. of unique gll points
+ngllx=5 #x축 상 GLL 점 개수
+ngllz=5 #z축 상 GLL 점 개수
+nxyz=ngllx*ngllz #한 요소의 총 GLL 점 개수
+ntot=nxyz*nspec #총 GLL 점 개수(중복 포함)
+nglob=(nelem_x*(ngllx-1)+1)*(nelem_z*(ngllz-1)+1) #총 고유 GLL 점 개수(중복 미포함)
 
-ngnod=4 #need 4 nodes to describe spectral element mesh
+ngnod=4 #4개의 node가 한 요소의 모양을 결정
 
 deltat=0.0011 #[s]
-nstep=1600 #no. of time steps
+nstep=1600 #시간 단계 개수
 nstep_between_output_info=100
 
 #Newmark-beta method
@@ -66,16 +66,16 @@ veloc=np.zeros(shape=(ndim,nglob))
 accel=np.zeros(shape=(ndim,nglob))
 seismogram=[]
 
-def endw1(n,alpha,beta): #xi=-1 end point weight
-    #important: n must be no. of gll points (ngll) - 1, consistent with Fortran code
+def endw1(n,alpha,beta): #xi=-1 GLL 점의 가중치
+    #중요: n은 GLL 점 개수(ngll)에서 1을 뺀 값, 원래 코드와 통일
     return 2**(alpha+beta+1)*(beta+1)*gamma(z=beta+1)**2*gamma(z=n)*gamma(z=n+alpha+1)/(gamma(z=n+beta+1)*gamma(z=n+alpha+beta+2))
 
-def endw2(n,alpha,beta): #xi=1 end point weight
-    #important: n must be no. of gll points (ngll) - 1, consistent with Fortran code
+def endw2(n,alpha,beta): #xi=1 GLL 점의 가중치
+    #중요: n은 GLL 점 개수(ngll)에서 1을 뺀 값, 원래 코드와 통일
     return 2**(alpha+beta+1)*(alpha+1)*gamma(z=alpha+1)**2*gamma(z=n)*gamma(z=n+beta+1)/(gamma(z=n+alpha+1)*gamma(z=n+alpha+beta+2))
 
-def zwgljd(n,alpha,beta): #zeros and weights of glj points
-    z,w=roots_jacobi(n=n-2,alpha=alpha+1,beta=beta+1) #zeros, weights
+def zwgljd(n,alpha,beta): #GLJ 근과 가중치
+    z,w=roots_jacobi(n=n-2,alpha=alpha+1,beta=beta+1) #Jacobi 다항식의 근과 가중치
     w/=1-z**2
     z=np.insert(arr=z,obj=[0,n-2],values=[-1,1]) #add -1 and 1
     w=np.insert(arr=w,obj=[0,n-2],values=[endw1(n=n-1,alpha=alpha,beta=beta),endw2(n=n-1,alpha=alpha,beta=beta)]) #add -1 and 1's weights
@@ -86,7 +86,7 @@ def define_derivation_matrices(zgll,ngll):
     #ngll: no. of gll points
     hprime=[] #h'
     index=0
-    while index<int(ngll**2/2): #because hprime is symmetric with just opposite sign, calculate only half
+    while index<int(ngll**2/2): #hprime은 부호만 반대일 뿐 점대칭 행렬이므로 반만 계산
         i=int(index/ngll) #row no.
         j=index%ngll #column no.
         #lagrange_deriv_GLL
@@ -98,7 +98,7 @@ def define_derivation_matrices(zgll,ngll):
             hprime.append(eval_legendre(n=ngll-1,x=zgll[i])/(eval_legendre(n=ngll-1,x=zgll[j])*(zgll[i]-zgll[j]))) #l_j'(xi_i)
         index+=1
 
-    if ngll%2: #ngll is odd
+    if ngll%2: #ngll이 홀수
         hprime.append(0)
 
     hprime=np.array(object=hprime)
@@ -112,27 +112,24 @@ hprime_zz=define_derivation_matrices(zgll=zigll,ngll=ngllz)
 hprimewgll_xx=hprime_xx*wxgll[:,np.newaxis] #(ngllx,ngllx) * (ngllx,1) = (ngllx,ngllx)
 hprimewgll_zz=hprime_zz*wzgll[:,np.newaxis] #(ngllz,ngllz) * (ngllz,1) = (ngllz,ngllz)
 
-#important: global node numbering,
-#starts at (xmin,zmin), increases x direction, consistent with Fortran code
+#중요: global 격자점 번호는 -ZBL 순서, 원래 코드와 통일
 nod_num=np.arange(stop=npgeo).reshape(nelem_z+1,nelem_x+1)
 
-#each spectral element's global node numbers
-#important: spectral element's order,
-#starts at bottom left, increases x direction, consistent with Fortran code
-#important: in case of 4 nodes,
-#xi coordinate (-1,-1) -> (1,-1) -> (1,1) -> (-1,1) order, consistent with Fortran code
+#각 요소를 이루는 격자점의 global 격자점 번호
+#중요: 요소 번호는 -ZBL 순서, 원래 코드와 통일
+#4개의 node일 경우 형상 함수는 xi 좌표 상 (-1,-1)->(1,-1)->(1,1)->(-1,1) 순서, 원래 코드와 통일
 knods[0]=nod_num[:-1,:-1].flatten() #(-1,-1)
 knods[1]=nod_num[:-1,1:].flatten() #(1,-1)
 knods[2]=nod_num[1:,1:].flatten() #(1,1)
 knods[3]=nod_num[1:,:-1].flatten() #(-1,1)
 
-#mapping global node number to node's real coordinate
+#global 격자점의 실제 좌표
 xrow=xmin+np.arange(stop=nelem_x+1)*(xmax-xmin)/nelem_x
-coorg[0]=np.tile(A=xrow,reps=nelem_z+1) #x coordinate
+coorg[0]=np.tile(A=xrow,reps=nelem_z+1) #x 좌표 [m]
 zcol=zmin+np.arange(stop=nelem_z+1)*(zmax-zmin)/nelem_z
-coorg[1]=np.repeat(a=zcol,repeats=nelem_x+1) #z coordinate
+coorg[1]=np.repeat(a=zcol,repeats=nelem_x+1) #z 좌표 [m]
 
-#define_shape_functions
+#형상 함수
 shape2D[0]=0.25*(xigll[:,np.newaxis]-1)*(zigll-1) #(-1,-1), (ngllx,1) + (ngllz) = (ngllx,ngllz)
 shape2D[1]=-0.25*(xigll[:,np.newaxis]+1)*(zigll-1) #(1,-1)
 shape2D[2]=0.25*(xigll[:,np.newaxis]+1)*(zigll+1) #(1,1)
@@ -178,7 +175,7 @@ decimals=smallvaltol-int(np.log10(xtypdist))
 xp=np.round(a=xp,decimals=decimals)
 zp=np.round(a=zp,decimals=decimals)
 
-#sort lexicographically, first by x coordinates, then by z to break tie
+#모든 GLL 점들(중복 포함)의 x 좌표로 먼저 정렬, 그 다음 같은 x 좌표들 내에서는 z 좌표로 정렬
 locval=np.lexsort(keys=(zp,xp))
 xp=xp[locval]
 zp=zp[locval]
@@ -197,18 +194,18 @@ if ig!=nglob-1:
 ibool=np.reshape(a=ibool,newshape=(nspec,ngllz,ngllx))
 ibool=np.swapaxes(a=ibool,axis1=0,axis2=2) #(ngllx,ngllz,nspec)
 
-#recompute_jacobian
+#Jacobian 계산
 temp_xxi=dershape2D_z[:,:,np.newaxis]*coorg[0,knods][:,np.newaxis] #(ngnod,ngllz,1) * (ngnod,1,nspec) = (ngnod,ngllz,nspec)
-xxi=sum(temp_xxi) #x/xi, (ngllz,nspec)
+xxi=sum(temp_xxi) #dx/dxi, (ngllz,nspec)
 
 temp_xxi=dershape2D_z[:,:,np.newaxis]*coorg[1,knods][:,np.newaxis]
-zxi=sum(temp_xxi) #z/xi
+zxi=sum(temp_xxi) #dz/dxi
 
 temp_xgamma=dershape2D_x[:,:,np.newaxis]*coorg[0,knods][:,np.newaxis] #(ngnod,ngllx,1) * (ngnod,1,nspec) = (ngnod,ngllx,nspec)
-xgamma=sum(temp_xgamma) #x/gamma, (ngllx,nspec)
+xgamma=sum(temp_xgamma) #dx/dgamma, (ngllx,nspec)
 
 temp_xgamma=dershape2D_x[:,:,np.newaxis]*coorg[1,knods][:,np.newaxis]
-zgamma=sum(temp_xgamma) #z/gamma
+zgamma=sum(temp_xgamma) #dz/dgamma
 
 jacobian=xxi*zgamma[:,np.newaxis]-xgamma[:,np.newaxis]*zxi #(ngllz,nspec) * (ngllx,1,nspec) = (ngllx,ngllz,nspec)
 
@@ -216,10 +213,10 @@ if (jacobian<=0).any():
     print("jacobian is less than or equal to 0")
     exit()
 
-xix=zgamma[:,np.newaxis]/jacobian #xi/x, (ngllx,1,nspec) / (ngllx,ngllz,nspec) = (ngllx,ngllz,nspec)
-gammax=-zxi/jacobian #gamma/x, (ngllz,nspec) / (ngllx,ngllz,nspec) = (ngllx,ngllz,nspec)
-xiz=-xgamma[:,np.newaxis]/jacobian #xi/z
-gammaz=xxi/jacobian #gamma/z
+xix=zgamma[:,np.newaxis]/jacobian #dxi/dx, (ngllx,1,nspec) / (ngllx,ngllz,nspec) = (ngllx,ngllz,nspec)
+gammax=-zxi/jacobian #dgamma/dx, (ngllz,nspec) / (ngllx,ngllz,nspec) = (ngllx,ngllz,nspec)
+xiz=-xgamma[:,np.newaxis]/jacobian #dxi/dz
+gammaz=xxi/jacobian #dgamma/dz
 
 for i in range(nspec):
     rmass_inverse[ibool[:,:,i]]+=wxgll[:,np.newaxis]*wzgll*rho*jacobian[:,:,i] #(ngllx,1) * (ngllz) * (ngllx,ngllz) = (ngllx,ngllz)
@@ -253,11 +250,11 @@ for it in range(nstep):
         temp_dux_dgamma=displ[1,ibool[:,:,i]][:,:,np.newaxis]*np.swapaxes(a=hprime_zz,axis1=0,axis2=1)
         duz_dgamma=np.sum(a=temp_dux_dgamma,axis=1) #du_z/dgamma
 
-        dux_dxl=dux_dxi*xix[:,:,i]+dux_dgamma*gammax[:,:,i] #du_x/dx, local
-        dux_dzl=dux_dxi*xiz[:,:,i]+dux_dgamma*gammaz[:,:,i] #du_x/dz, local
+        dux_dxl=dux_dxi*xix[:,:,i]+dux_dgamma*gammax[:,:,i] #du_x/dx
+        dux_dzl=dux_dxi*xiz[:,:,i]+dux_dgamma*gammaz[:,:,i] #du_x/dz
 
-        duz_dxl=duz_dxi*xix[:,:,i]+duz_dgamma*gammax[:,:,i] #du_z/dx, local
-        duz_dzl=duz_dxi*xiz[:,:,i]+duz_dgamma*gammaz[:,:,i] #du_z/dz, local
+        duz_dxl=duz_dxi*xix[:,:,i]+duz_dgamma*gammax[:,:,i] #du_z/dx
+        duz_dzl=duz_dxi*xiz[:,:,i]+duz_dgamma*gammaz[:,:,i] #du_z/dz
 
         sigma_xx=lambdaplus2mu*dux_dxl+lambda_*duz_dzl
         sigma_xz=mu*(duz_dxl+dux_dzl)
