6bd7070088f6375e8fd36d71f446466a7957a9d3
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-06-27 17:01:06 +0900

    Update noise_orientation.py

--- noise_orientation.py
+++ noise_orientation.py
@@ -1,6 +1,7 @@
 from datetime import datetime,timedelta
 from os.path import exists
 from os import mkdir
+from pprint import pprint
 
 import numpy as np
 import numpy.ma as ma
@@ -30,31 +31,61 @@ win_overlap=0.5
 win_no=0
 max_subwin=int(win_length/(subwin_length*(1-subwin_overlap)))
 next_subwin=int(win_length*(1-subwin_overlap)/(subwin_length*(1-subwin_overlap)))
+n_close=20
 
 julday_ls=0
 nsubwin_ls=[]
-C_ZZ={}
-C_NZ={}
-C_EZ={}
-st=Stream()
+st_Z=Stream()
+st_N=Stream()
+st_E=Stream()
 
-net=read_inventory(path_or_file_object="stameta_NS",format="STATIONTXT",level="station")[0] #network
-code_idx={}
-for i in range(len(net)):
-    code_idx[net[i].code]=i
+def preprocess(code,component):
+    if not exists(path=f"/home/tllc46/NSNAS/{.code}/HH{component}/NS.{code}.HH{component}.{subwin_end.year}.{subwin_end.julday:03}"): #miniseed 파일이 없다
+        file.write(f"{code} {component} | no data\n")
+        return None #제외
+    else: #miniseed 파일 존재
+        st_temp=read(pathname_or_url=f"/home/tllc46/NSNAS/{code}/HH{component}/*{subwin_end.year}.{subwin_end.julday:03}",format="MSEED",header_byteorder=">")
+        st_temp.merge(method=1)
+        tr_temp=st_temp[0]
+        if type(tr_temp.data)==ma.MaskedArray: #공백 존재
+            file.write(f"{code} {component} | discontinous data\n")
+            return None #제외
+        tr_temp.decimate(factor=int(tr_temp.stats.sampling_rate/sampling_rate),no_filter=True) #decimate
+        tr_temp.filter(type="bandpass",freqmin=freqmin,freqmax=freqmax,zerophase=True) #대역 통과 필터
+        if tr_temp.stats.component=="X": #성분 이름 수정
+            tr_temp.stats.component="E"
+        elif tr_temp.stats.component=="Y":
+            tr_temp.stats.component="N"
+        return tr_temp
 
-r_s_idx=np.zeros(shape=(len(net),len(net)),dtype=int)
+def new_julday():
+    global st_Z,st_N,st_E
+    file.write("reading new data start\n")
+    for i in range(len(net)): #각 지진계마다
+        tr_temp=preprocess(code=net[i].code,component="Z")
+        if not tr_temp: #Z축 성분이 존재하지 않다
+            file.write("
+            continue
+        else:
+            st_Z+=tr_temp
 
-for i in range(len(net)):
-    r_idx=code_idx[net[i].code] #지진계
-    gcarc_ls=[]
-    for j in range(len(net)):
-        if i!=j:
-            gcarc_ls.append([gps2dist_azimuth(lat1=net[i].latitude,lon1=net[i].longitude,lat2=net[j].latitude,lon2=net[j].longitude)[0],net[j].code])
-    gcarc_ls.sort()
-    for j in range(20): #i번째 지진계와 가장 가까운 20개 지진계만
-        s_idx=code_idx[gcarc_ls[j][1]] #source
-        r_s_idx[r_idx,s_idx]=j+1
+        tr_temp=preprocess(code=net[i].code,componenet="N")
+        if not tr_temp: #N축 성분이 존재하지 않다
+            continue
+        else:
+            st_N+=tr_temp
+
+        tr_temp=preprocess(code=net[i].code,component="E")
+        if tr_temp:
+            st_E+=tr_temp
+    file.write("reading new data end\n")
+            
+    st_Z.merge()
+    st_Z.trim(starttime=subwin_start,endtime=UTCDateTime(year=subwin_end.year,julday=subwin_end.julday)+86400-delta,pad=True)
+    st_N.merge()
+    st_N.trim(starttime=subwin_start,endtime=UTCDateTime(year=subwin_end.year,julday=subwin_end.julday)+86400-delta,pad=True)
+    st_E.merge()
+    st_E.trim(starttime=subwin_start,endtime=UTCDateTime(year=subwin_end.year,julday=subwin_end.julday)+86400-delta,pad=True)
 
 def fourier_trans(data):
     if type(data)==ma.MaskedArray:
@@ -72,50 +103,6 @@ def cross_correlate(spectrum1,spectrum2):
     corr=np.fft.irfft(a=corr_spec) #역 Fourier 변환
     corr=np.real(np.concatenate((corr[-nlag:],corr[:nlag+1]))) #time lag까지만 자르다
     return corr
-
-def preprocess(code,component):
-    if not exists(path=f"/home/tllc46/NSNAS/{net[i].code}/HH{component}/NS.{net[i].code}.HH{component}.{subwin_end.year}.{subwin_end.julday:03}"): #miniseed 파일이 없다
-        return None #제외
-    else: #miniseed 파일 존재
-        st_temp=read(pathname_or_url=f"/home/tllc46/NSNAS/{code}/HH{component}/*{subwin_end.year}.{subwin_end.julday:03}",format="MSEED",header_byteorder=">")
-        print(f"done reading {code}")
-        st_temp.merge(method=1)
-        tr_temp=st_temp[0]
-        if type(tr_temp.data)==ma.MaskedArray: #공백 존재
-            return None #제외
-        tr_temp.decimate(factor=int(tr_temp.stats.sampling_rate/sampling_rate),no_filter=True) #decimate
-        tr_temp.filter(type="bandpass",freqmin=freqmin,freqmax=freqmax,zerophase=True) #대역 통과 필터
-        if tr_temp.stats.component=="X": #성분 이름 수정
-            tr_temp.stats.component="E"
-        elif tr_temp.stats.component=="Y":
-            tr_temp.stats.component="N"
-        return tr_temp
-
-def new_julday():
-    global st_Z,st_N,st_E
-    for i in range(len(net)): #각 지진계마다
-        tr_temp=preprocess(code=net[i].code,component="Z")
-        if not tr_temp: #Z축 성분이 존재하지 않다
-            continue
-        else:
-            st_Z+=tr_temp
-
-        tr_temp=preprocess(code=net[i].code,componenet="N")
-        if not tr_temp: #N축 성분이 존재하지 않다
-            continue
-        else:
-            st_N+=tr_temp
-
-        tr_temp=preprocess(code=net[i].code,component="E")
-        if tr_temp:
-            st_E+=tr_temp
-            
-    st_Z.merge()
-    st_Z.trim(starttime=subwin_start,endtime=UTCDateTime(year=subwin_end.year,julday=subwin_end.julday)+86400-delta,pad=True)
-    st_N.merge()
-    st_N.trim(starttime=subwin_start,endtime=UTCDateTime(year=subwin_end.year,julday=subwin_end.julday)+86400-delta,pad=True)
-    st_E.merge()
-    st_E.trim(starttime=subwin_start,endtime=UTCDateTime(year=subwin_end.year,julday=subwin_end.julday)+86400-delta,pad=True)
 
 def C_new_win():
     global nsubwin_ls,C_ZZ,C_NZ,C_EZ
@@ -159,13 +146,40 @@ def C_old_win():
             sac.user0=subwin[1]
             sac.write(dest=f"/media/tllc46/data01/Orientation/{win_no:02}/EZ/{i}.SAC")
 
+file=open(file="debug.txt",mode="w")
+
+net=read_inventory(path_or_file_object="stameta_NS",format="STATIONTXT",level="station")[0] #network
+n_sta=len(net)
+code_idx={}
+for i in range(n_sta):
+    code_idx[net[i].code]=i
+file.write("station code - index mapping\n")
+pprint(object=code_idx,stream=file)
+file.write("\n")
+
+r_s_idx=np.zeros(shape=(n_sta,n_sta),dtype=int)
+for i in range(n_sta):
+    r_idx=code_idx[net[i].code] #지진계
+    gcarc_ls=[]
+    for j in range(n_sta):
+        if i!=j:
+            gcarc_ls.append([gps2dist_azimuth(lat1=net[i].latitude,lon1=net[i].longitude,lat2=net[j].latitude,lon2=net[j].longitude)[0],net[j].code])
+    gcarc_ls.sort()
+    for j in range(n_close): #i번째 지진계와 가장 가까운 20개 지진계만
+        s_idx=code_idx[gcarc_ls[j][1]] #source
+        r_s_idx[r_idx,s_idx]=j+1
+file.write("station pairs - index mapping\n")
+pprint(object=r_s_idx,stream=file)
+file.write("\n")
+
 C_new_win()
 
 while subwin_end<=endtime:
-    print(f"sub-window starts: {subwin_start} ~ {subwin_end}")
+    file.write(f"new subwindow starts | {subwin_start} ~ {subwin_end}\n")
     #새 trace 추가
     if subwin_end.julday!=julday_ls: #새 Julian일 시작
         julday_ls=subwin_end.julday
+        file.write(f"new Julian day starts | {julday_ls}\n")
         new_julday()
 
     #부구간으로만 자르다
@@ -182,29 +196,36 @@ while subwin_end<=endtime:
                 continue
             j+=1
 
+    n_tr_Z=len(st_subwin_Z)
+    n_tr_N=len(st_subwin_N)
+    n_tr_E=len(st_subwin_E)
+
     #sanity check
-    for i in range(len(st_subwin_Z)):
+    for i in range(n_tr_Z):
         r_idx=code_idx[st_subwin_Z[i].stats.station]
-        for j in range(i+1,len(st_subwin_Z)):
+        for j in range(i+1,n_tr_Z):
+            sym=0
             s_idx=code_idx[st_subwin_Z[j].stats.station]
             if r_s_idx[r_idx,s_idx]:
-                subwin_idx_Z[r_idx,r_s_idx[r_idx,s_idx]-1]=[i,j]
+                subwin_idx_Z[r_idx,r_s_idx[r_idx,s_idx]-1]=[i+1,j+1]
                 sym+=1
             if r_s_idx[s_idx,r_idx]:
-                subwin_idx_Z[s_idx,r_s_idx[s_idx,r_idx]-1]=[j,i]
+                subwin_idx_Z[s_idx,r_s_idx[s_idx,r_idx]-1]=[i+1,j+1]
                 sym+=1
             if sym==2:
                 sym_flag[s_idx,r_s_idx[s_idx,r_idx]-1]=True
-
-    for i in range(len(st_subwin_N)):
-        r_idx=code_idx[st_subwin_N[i].stats.station]
-        for j in range(i+1,len(st_subwin_N)):
+        for j in range(n_tr_N):
             s_idx=code_idx[st_subwin_N[j].stats.station]
-            if r_s_idx[r_idx,s_idx]:
-                subwin_idx_Z[r_idx,r_s_idx[r_idx,s_idx]-1]=[i,j]
             if r_s_idx[s_idx,r_idx]:
-                subwin_idx_Z[s_idx,r_s_idx[s_idx,r_idx]-1]=[j,i]
+                subwin_idx_N[s_idx,r_s_idx[s_idx,r_idx]-1]=[j+1,i+1]
+        for j in range(n_tr_E):
+            s_idx=code_idx[st_subwin_E[j].stats.station]
+            if r_s_idx[s_idx,r_idx]:
+                subwin_idx_E[s_idx,r_s_idx[s_idx,r_idx]-1]=[j+1,i+1]
 
+    for i in range(n_sta):
+        for j in range(n_close):
+            if not subwin_idx_Z[i,j] 
                 
     #cross correlate
     for i in range(len(vertical)):
@@ -259,3 +280,5 @@ while subwin_end<=endtime:
 
     subwin_start+=subwin_length*(1-win_overlap)
     subwin_end+=subwin_length*(1-win_overlap)
+
+file.close()
