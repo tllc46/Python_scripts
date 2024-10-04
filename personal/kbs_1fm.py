43d38e2d83d6844922be543f5ba87c601d3b45f6
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-10-05 01:10:09 +0900

    Update kbs_1fm.py

--- personal/kbs_1fm.py
+++ personal/kbs_1fm.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 start_url="https://pbbs.kbs.co.kr/general/read.html?bbs_id=R2002-0281-03-240543&id=1789866&post_no=4206&page=1&post_header="
-file=open(file="C:/Users/tllc46/Downloads/song_list.txt",mode="w",encoding="utf-8")
+file=open(file="song_list_2.txt",mode="w",encoding="utf-8")
 driver=webdriver.Chrome()
 driver.get(url=start_url)
 
@@ -13,10 +13,7 @@ while True:
         sleep(1)
         elem=driver.find_element(by=By.XPATH,value="/html/body/div/div[2]/div/div[4]/div[1]")
         text=elem.text
-        songs=text.split(sep="\n\n")
-        for song in songs:
-            if "Telemann" in song or "텔레만" in song:
-                file.write(song+"\n\n")
+        file.write(text+"\n")
         elem=driver.find_element(by=By.XPATH,value="/html/body/div/div[2]/div/div[4]/div[5]/ul/li[2]/div[3]/a")
         next_url=elem.get_attribute(name="href")
         driver.get(url=next_url)
