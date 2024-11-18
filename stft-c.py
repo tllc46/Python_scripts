64a14c9a880545e5b4de584dd3c9f254996a0d3b
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-11-18 16:59:14 +0900

    Update stft-c.py

--- stft-c.py
+++ stft-c.py
@@ -4,14 +4,14 @@ import numpy as np
 
 from obspy import read
 
-st=read("05.SS05.HHZ.2013.304",format="MSEED",header_byteorder="<")
+st=read(pathname_or_url="05.SS05.HHZ.2013.304",format="MSEED",header_byteorder="<")
 data=st[0].data[:360000]
 
 nperseg=40
 
 #stft
-fac=np.arange(nperseg+1)
-win=0.5*(1-np.cos(2*np.pi*fac/(nperseg)))[:-1]
+fac=np.arange(stop=nperseg+1)
+win=0.5*(1-np.cos(2*np.pi*fac/nperseg))[:-1]
 nfft=nperseg
 noverlap=nperseg//2
 nstep=nperseg-noverlap
@@ -24,14 +24,14 @@ nseg=(pad_length-nperseg)//nstep+1
 result=np.empty(shape=(nfft//2+1,nseg),dtype=complex)
 for i in range(nseg):
     segment=x[i*nstep:i*nstep+nperseg]*win
-    result[:,i]=np.fft.rfft(segment)
+    result[:,i]=np.fft.rfft(a=segment)
 result*=scale
 
 #istft
 x=np.zeros(shape=pad_length)
 norm=np.zeros(shape=pad_length)
 for i in range(nseg):
-    xsubs=np.fft.irfft(result[:,i])
+    xsubs=np.fft.irfft(a=result[:,i])
     xsubs*=0.5*nperseg
     x[i*nstep:i*nstep+nperseg]+=xsubs*win
     norm[i*nstep:i*nstep+nperseg]+=win**2
