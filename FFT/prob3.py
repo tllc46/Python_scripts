a276557f56977e6a7d3106a15c391e8ec82896a2
tllc46 <155712238+tllc46@users.noreply.github.com>
2025-03-05 10:37:17 +0900

    Update prob3.py

--- FFT/prob3.py
+++ FFT/prob3.py
@@ -23,7 +23,7 @@ analy*=0.1*np.pi #정규화
 seq=np.arange(stop=N)
 seq_rect=np.concatenate((seq[:int(0.5*tau)+1],seq[-int(0.5*tau):]))
 complex_dft=A*sum(np.exp(-1j*2*np.pi*seq*seq_rect[:,np.newaxis]/N))
-dft=0.1*np.pi*complex_dft.real #normalization
+dft=0.1*np.pi*complex_dft.real #정규화
 dft=np.concatenate((dft[int(0.5*N):],dft[:int(0.5*N)+1])) #omega 범위가 -pi ~ pi가 되도록 재배열
 
 fig=plt.figure()
