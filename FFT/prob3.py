2bef5552093fc5efdb7570cdc097ae700abb4129
tllc46 <155712238+tllc46@users.noreply.github.com>
2025-03-05 13:10:03 +0900

    Update prob3.py

--- FFT/prob3.py
+++ FFT/prob3.py
@@ -21,8 +21,10 @@ analy=np.array(object=[analytic(omega=omega_) for omega_ in omega])
 analy*=0.1*np.pi #정규화
 
 seq=np.arange(stop=N)
-seq_rect=np.concatenate((seq[:int(0.5*tau)+1],seq[-int(0.5*tau):]))
-complex_dft=A*sum(np.exp(-1j*2*np.pi*seq*seq_rect[:,np.newaxis]/N))
+if int(0.5*tau)<1:
+    seq_rect=np.array([0])
+else:
+    seq_rect=np.concatenate((seq[:int(0.5*tau)+1],seq[-int(0.5*tau):]))complex_dft=A*sum(np.exp(-1j*2*np.pi*seq*seq_rect[:,np.newaxis]/N))
 dft=0.1*np.pi*complex_dft.real #정규화
 dft=np.concatenate((dft[int(0.5*N):],dft[:int(0.5*N)+1])) #omega 범위가 -pi ~ pi가 되도록 재배열
 
