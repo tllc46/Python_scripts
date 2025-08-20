5ac5e183862a4f5de19431798ffd01489089937d
tllc46 <155712238+tllc46@users.noreply.github.com>
2025-08-21 00:18:13 +0900

    Update prob2.py

--- FFT/prob2.py
+++ FFT/prob2.py
@@ -5,7 +5,7 @@ A=1
 tau=20
 T=256
 dt=1
-N=int(T/dt)
+N=T//dt
 
 omega_analy=np.arange(stop=int(6*np.pi/0.01)+1)*0.01-3*np.pi
 omega_dft=np.arange(stop=3*N+1)*2*np.pi/N-3*np.pi
@@ -13,10 +13,10 @@ omega_dft=np.arange(stop=3*N+1)*2*np.pi/N-3*np.pi
 analy=2*A*np.sin(0.5*tau*omega_analy)/omega_analy
 
 seq=np.arange(stop=N)
-seq_rect=np.concatenate((seq[:int(0.5*tau)+1],seq[-int(0.5*tau):]))
+seq_rect=np.concatenate((seq[:tau//2+1],seq[-(tau//2):]))
 complex_dft=A*sum(np.exp(-1j*2*np.pi*seq*seq_rect[:,np.newaxis]/N))
 dft=complex_dft.real
-dft=np.concatenate((dft[int(0.5*N):],dft[:int(0.5*N)])) #omega 범위가 -pi ~ pi가 되도록 재배열
+dft=np.concatenate((dft[N//2:],dft[:N//2])) #omega 범위가 -pi ~ pi가 되도록 재배열
 dft=np.tile(A=dft,reps=3) #omega 범위가 -3*pi ~ 3*pi가 되도록 3회 반복
 dft=np.append(arr=dft,values=dft[0]) #omega=-3*pi의 요소를 뒤에 추가
 
