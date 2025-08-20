579c8756570a64ceb10339c01ccac1907865b979
tllc46 <155712238+tllc46@users.noreply.github.com>
2025-08-21 00:19:53 +0900

    Update prob3.py

--- FFT/prob3.py
+++ FFT/prob3.py
@@ -5,7 +5,7 @@ A=10
 tau=2
 dT=20
 dt=1
-N=int(dT/dt)
+N=dT//dt
 
 def analytic(omega):
     if omega:
@@ -21,12 +21,12 @@ analy=np.array(object=[analytic(omega=omega_) for omega_ in omega])
 analy*=0.1*np.pi #정규화
 
 seq=np.arange(stop=N)
-if int(0.5*tau)<1:
+if int(tau/2)<1:
     seq_rect=np.array([0])
 else:
-    seq_rect=np.concatenate((seq[:int(0.5*tau)+1],seq[-int(0.5*tau):]))complex_dft=A*sum(np.exp(-1j*2*np.pi*seq*seq_rect[:,np.newaxis]/N))
+    seq_rect=np.concatenate((seq[:int(tau/2)+1],seq[-int(tau/2):]))complex_dft=A*sum(np.exp(-1j*2*np.pi*seq*seq_rect[:,np.newaxis]/N))
 dft=0.1*np.pi*complex_dft.real #정규화
-dft=np.concatenate((dft[int(0.5*N):],dft[:int(0.5*N)+1])) #omega 범위가 -pi ~ pi가 되도록 재배열
+dft=np.concatenate((dft[N//2:],dft[:N//2+1])) #omega 범위가 -pi ~ pi가 되도록 재배열
 
 fig=plt.figure()
 ax=fig.subplots()
