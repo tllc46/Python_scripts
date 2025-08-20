2455286cca99ad078bc65c030f8ce62a49ae3b52
tllc46 <155712238+tllc46@users.noreply.github.com>
2025-08-21 00:02:35 +0900

    Update vesp.py

--- vesp.py
+++ vesp.py
@@ -54,7 +54,7 @@ geometry=geometry[:-1]
 _,_,baz=gps2dist_azimuth(lat1=st[0].stats.sac.evla,lon1=st[0].stats.sac.evlo,lat2=center[1],lon2=center[0])
 
 delta=st[0].stats.delta
-time=np.arange(stop=(tend-tbegin)//delta+1)*delta+tbegin
+time=np.arange(stop=int((tend-tbegin)/delta)+1)*delta+tbegin
 slow=np.arange(stop=nslow+1)*(maxslow-minslow)/nslow+minslow
 
 #beamforming
